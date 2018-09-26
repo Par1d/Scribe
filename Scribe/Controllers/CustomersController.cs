20c9961b4e61d3a1162dd3bc804e6d671dbd12ab
David Henning <david.f.henning@gmail.com>
2018-09-25 21:03:43 -0600

    Added Initial Customer View

---
+++ Scribe/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scribe.Models;
+
+namespace Scribe.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            List<Customer> data = new List<Customer>
+            {
+                new Customer() {name = "ABC Company", city = "Billings", state = "Montana" },
+                new Customer() {name = "Yellowstone National Park Foundation", city = "Cody", state = "Wyoming" },
+                new Customer() {name = "360 Office Solutions", city = "Helena", state = "Montana"}
+            };
+
+            return View("CustomerList", data);
+        }
+    }
+}
---
+++ Scribe/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scribe.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+    }
+}
